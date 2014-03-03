f855523e9e0259f2be5b3b99cca56200d82eb3a9
Eduardo Tenório <embatbr@gmail.com>
2014-03-03 03:38:54 -0300

    Writing post neural-networks-in-a-nutshell
    Almost done, just need to write the McCulloch-Pitts code in Python.

---
+++ posts/neural-networks-in-a-nutshell/code/mcp.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+
+"""author: Eduardo Tenório (embatbr@gmail.com)
+The Men Who Stare at Codes (themenwhostareatcodes.wordpress.com)
+
+This code is intended to show how to define a McCulloch-Pitts neuron.
+"""
