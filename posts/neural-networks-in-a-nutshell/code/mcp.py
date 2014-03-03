925af6f4208bb7978aa33b31b997a71f8e745427
Eduardo Tenório <embatbr@gmail.com>
2014-03-03 14:59:58 -0300

    Post neural-networks-in-a-nutshell published

--- posts/neural-networks-in-a-nutshell/code/mcp.py
+++ posts/neural-networks-in-a-nutshell/code/mcp.py
@@ -4,5 +4,35 @@
 """author: Eduardo Tenório (embatbr@gmail.com)
 The Men Who Stare at Codes (themenwhostareatcodes.wordpress.com)
 
-This code is intended to show how to define a McCulloch-Pitts neuron.
+This code is intended to show how to define and use a McCulloch-Pitts neuron.
 """
+
+
+def heaviside(x):
+    if x >= 0:
+        return 1
+    else:
+        return 0
+
+
+class MCP_Neuron(object):
+
+    def __init__(self, weights, bias):
+        self.weights = weights
+        self.bias = bias
+
+    def fire(self, inputs):
+        summed = sum([i*w for (i,w) in zip(inputs, self.weights)])
+        return heaviside(summed + self.bias)
+
+
+if __name__ == '__main__':
+    neuron_1 = MCP_Neuron([0.2, 0.7, 0.3], -1.5)
+    neuron_2 = MCP_Neuron([0.4, 0.6, 0.9], -0.8)
+    neuron_3 = MCP_Neuron([0.7, 0.4, -0.9], -0.6)
+
+    inputs = [1, 0, 1]
+
+    print('Test #1 - inputs on neuron_1:', neuron_1.fire(inputs))
+    print('Test #1 - inputs on neuron_2:', neuron_2.fire(inputs))
+    print('Test #1 - inputs on neuron_3:', neuron_3.fire(inputs))
